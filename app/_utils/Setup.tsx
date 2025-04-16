df572156b586c2e674cb6f54d758738cb917ad0b
Mateusz Pazdan <pazdan.mateuszz@gmail.com>
2025-04-16 18:14:13 +0200

    settings of QueryClinet

--- app/_utils/Setup.tsx
+++ app/_utils/Setup.tsx
@@ -8,7 +8,16 @@ interface SetupProps {
 }
 
 export default function Setup({ children }: SetupProps) {
-	const queryClient = new QueryClient();
+	const queryClient = new QueryClient({
+		defaultOptions: {
+			queries: {
+				staleTime: 1000 * 60 * 5,
+				refetchOnWindowFocus: false,
+				retry: 1,
+			},
+		},
+	});
+
 	return (
 		<QueryClientProvider client={queryClient}>
 			{children}

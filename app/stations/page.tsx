acdfdb0512223cadc798ce7dc6289555022e6d67
Mateusz Pazdan <pazdan.mateuszz@gmail.com>
2025-04-22 16:23:54 +0200

    add link to go back homepage

--- app/stations/page.tsx
+++ app/stations/page.tsx
@@ -2,6 +2,7 @@ import StationsFilters from '../_components/stations/StationsFilters';
 import StationsContainer from '../_components/stations/StationsContainer';
 import { Suspense } from 'react';
 import Spinner from '../_components/Spinner';
+import Link from 'next/link';
 
 export const VOIVODESHIPS = [
 	'PODKARPACKIE',
@@ -35,6 +36,12 @@ interface PageProps {
 export default async function page({ searchParams }: PageProps) {
 	return (
 		<main className='p-6'>
+			<Link
+				className='text-blue-500 hover:underline mb-4 inline-block'
+				href={'/'}
+			>
+				← Powrót do strony głównej
+			</Link>
 			<h1 className='text-2xl font-bold mb-4'>Lista stacji</h1>
 			<StationsFilters searchParams={searchParams} />
 			<Suspense

540cb517dd5724a4bdb7e2e9c6825049ace91711
Mateusz Pazdan <pazdan.mateuszz@gmail.com>
2025-04-29 13:36:11 +0200

    add close button to StationDetailsModal.tsx

--- app/_components/Map.tsx
+++ app/_components/Map.tsx
@@ -1,135 +1,139 @@
-import { Dispatch, SetStateAction, useEffect, useState } from "react";
-import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
-import "leaflet/dist/leaflet.css";
-import L from "leaflet";
-import { getStationsWithFiltersClient } from "../lib/getStationsWithFiltersClient";
-import { Station } from "../_actions/stationActions";
-import { StationsSearchParams } from "../stations/page";
-import { ClipLoader, PulseLoader } from "react-spinners";
-import Modal from "./ui/Modal";
-import StationDetailsModal from "./StationDetailsModal";
+import { Dispatch, SetStateAction, useEffect, useState } from 'react';
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import 'leaflet/dist/leaflet.css';
+import L from 'leaflet';
+import { getStationsWithFiltersClient } from '../lib/getStationsWithFiltersClient';
+import { Station } from '../_actions/stationActions';
+import { StationsSearchParams } from '../stations/page';
+import { ClipLoader, PulseLoader } from 'react-spinners';
+import Modal from './ui/Modal';
+import StationDetailsModal from './StationDetailsModal';
 
 // Fix ikon w Leaflet
 delete (L.Icon.Default as any).prototype._getIconUrl;
 L.Icon.Default.mergeOptions({
-  iconRetinaUrl:
-    "https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon-2x.png",
-  iconUrl: "https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon.png",
-  shadowUrl: "https://unpkg.com/leaflet@1.9.4/dist/images/marker-shadow.png",
+	iconRetinaUrl:
+		'https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon-2x.png',
+	iconUrl: 'https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon.png',
+	shadowUrl: 'https://unpkg.com/leaflet@1.9.4/dist/images/marker-shadow.png',
 });
 
 interface MapProps {
-  params: StationsSearchParams;
+	params: StationsSearchParams;
 }
 
 export default function Map({ params }: MapProps) {
-  const [stations, setStations] = useState<Station[]>([]);
-  const [initialLoading, setInitialLoading] = useState(true);
-  const [loading, setLoading] = useState(true);
+	const [stations, setStations] = useState<Station[]>([]);
+	const [initialLoading, setInitialLoading] = useState(true);
+	const [loading, setLoading] = useState(true);
 
-  useEffect(() => {
-    const controller = new AbortController();
-    const signal = controller.signal;
+	useEffect(() => {
+		const controller = new AbortController();
+		const signal = controller.signal;
 
-    setStations([]);
-    setInitialLoading(true);
-    setLoading(true);
+		setStations([]);
+		setInitialLoading(true);
+		setLoading(true);
 
-    const fetchAllStations = async () => {
-      try {
-        const seenIds = new Set<number>();
+		const fetchAllStations = async () => {
+			try {
+				const seenIds = new Set<number>();
 
-        while (true) {
-          const data = await getStationsWithFiltersClient(
-            {
-              ...params,
-            },
-            signal
-          );
+				while (true) {
+					const data = await getStationsWithFiltersClient(
+						{
+							...params,
+						},
+						signal
+					);
 
-          const newItems = data.items.filter((item) => !seenIds.has(item.id));
-          newItems.forEach((item) => seenIds.add(item.id));
+					const newItems = data.items.filter((item) => !seenIds.has(item.id));
+					newItems.forEach((item) => seenIds.add(item.id));
 
-          setStations((prev) => {
-            const newStations = data.items.filter(
-              (station) => !prev.some((s) => s.id === station.id)
-            );
-            return [...prev, ...newStations];
-          });
+					setStations((prev) => {
+						const newStations = data.items.filter(
+							(station) => !prev.some((s) => s.id === station.id)
+						);
+						return [...prev, ...newStations];
+					});
 
-          if (params.page === 1) {
-            setInitialLoading(false);
-            setLoading(true);
-          }
+					if (params.page === 1) {
+						setInitialLoading(false);
+						setLoading(true);
+					}
 
-          if (params.page! >= data.pages) break;
-          params.page!++;
-        }
-      } catch (err) {
-        console.error("Błąd podczas ładowania stacji:", err);
-      } finally {
-        setLoading(false);
-      }
-    };
+					if (params.page! >= data.pages) break;
+					params.page!++;
+				}
+			} catch (err) {
+				console.error('Błąd podczas ładowania stacji:', err);
+			} finally {
+				setLoading(false);
+			}
+		};
 
-    fetchAllStations();
+		fetchAllStations();
 
-    return () => {
-      controller.abort();
-      setStations([]);
-      setInitialLoading(true);
-      setLoading(true);
-    };
-  }, [params]);
+		return () => {
+			controller.abort();
+			setStations([]);
+			setInitialLoading(true);
+			setLoading(true);
+		};
+	}, [params]);
 
-  return (
-    <div className="flex justify-center items-center p-4 w-full h-full ">
-      <Modal>
-        <div className="w-full h-full flex justify-center items-center rounded-lg shadow-2xl overflow-hidden bg-white">
-          {initialLoading ? (
-            <div className="flex justify-center items-center w-full h-full">
-              <PulseLoader size={40} color="#3b82f6 " />
-            </div>
-          ) : (
-            <div className="relative w-full h-full ">
-              <MapContainer
-                center={[52.2297, 21.0122]}
-                zoom={6}
-                scrollWheelZoom
-                style={{ height: "100%", width: "100%" }}
-              >
-                <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
-                {stations.map((station) => (
-                  <Marker
-                    key={station.id}
-                    position={[station.latitude, station.longitude]}
-                  >
-                    <Popup>
-                      <div className="flex flex-col items-start">
-                        <strong>{}</strong>
-                        {station.city}, {station.address}
-                        <Modal.Open opens={`stationDetails ${station.id}`}>
-                          <button className="mt-2 text-blue-500 hover:underline">
-                            Zobacz szczegóły
-                          </button>
-                        </Modal.Open>
-                        <Modal.Window name={`stationDetails ${station.id}`}>
-                          <StationDetailsModal stationCode={station.code} station={station} />
-                        </Modal.Window>
-                      </div>
-                    </Popup>
-                  </Marker>
-                ))}
-              </MapContainer>
-              {loading && (
-                <div className="absolute top-0 left-0 w-full h-full flex justify-center items-center bg-black bg-opacity-50 z-[9999]">
-                  <PulseLoader size={40} color="#fff" />
-                </div>
-              )}
-            </div>
-          )}
-        </div>
-      </Modal>
-    </div>
-  );
+	return (
+		<div className='flex justify-center items-center p-4 w-full h-full '>
+			<Modal>
+				<div className='w-full h-full flex justify-center items-center rounded-lg shadow-2xl overflow-hidden bg-white'>
+					{initialLoading ? (
+						<div className='flex justify-center items-center w-full h-full'>
+							<PulseLoader size={40} color='#3b82f6 ' />
+						</div>
+					) : (
+						<div className='relative w-full h-full '>
+							<MapContainer
+								center={[52.2297, 21.0122]}
+								zoom={6}
+								scrollWheelZoom
+								style={{ height: '100%', width: '100%' }}
+							>
+								<TileLayer url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png' />
+								{stations.map((station) => (
+									<Marker
+										key={station.id}
+										position={[station.latitude, station.longitude]}
+									>
+										<Popup>
+											<div className='flex flex-col items-start'>
+												<strong>{}</strong>
+												{station.city}, {station.address}
+												<Modal.Open opens={`stationDetails ${station.id}`}>
+													<button className='mt-2 text-blue-500 hover:underline'>
+														Zobacz szczegóły
+													</button>
+												</Modal.Open>
+												<Modal.Window name={`stationDetails ${station.id}`}>
+													<StationDetailsModal
+														stationCode={station.code}
+														station={station}
+														onCloseModal={() => undefined}
+													/>
+												</Modal.Window>
+											</div>
+										</Popup>
+									</Marker>
+								))}
+							</MapContainer>
+							{loading && (
+								<div className='absolute top-0 left-0 w-full h-full flex justify-center items-center bg-black bg-opacity-50 z-[9999]'>
+									<PulseLoader size={40} color='#fff' />
+								</div>
+							)}
+						</div>
+					)}
+				</div>
+			</Modal>
+		</div>
+	);
 }
--- app/_components/StationDetailsModal.tsx
+++ app/_components/StationDetailsModal.tsx
@@ -2,10 +2,12 @@ import React from 'react';
 import { useSensors } from '../_hooks/useSensors';
 import { Station } from '../_actions/stationActions';
 import Link from 'next/link';
+import { IoClose } from 'react-icons/io5';
 
 interface StationDetailsModalProps {
 	stationCode: string;
 	station: Station;
+	onCloseModal: () => void;
 }
 
 function formatDate(timestamp?: string): string {
@@ -24,6 +26,7 @@ function formatDate(timestamp?: string): string {
 export default function StationDetailsModal({
 	stationCode,
 	station,
+	onCloseModal,
 }: StationDetailsModalProps) {
 	const { data: stationsData } = useSensors({
 		station_code: stationCode,
@@ -33,43 +36,50 @@ export default function StationDetailsModal({
 	});
 
 	return (
-		<div className="p-6 mx-auto">
-			<h1 className="text-2xl font-semibold text-gray-800 mb-4">
-				{station.name}
-			</h1>
-
-			<div>
+		<div className='px-6 py-2 mx-auto'>
+			<div className='flex flex-row justify-between items-center mb-4'>
+				<h1 className='text-2xl font-semibold text-gray-800'>
+					{station.name}
+				</h1>
+				<button
+					className='p-2 text-3xl hover:bg-gray-50 self-end'
+					onClick={onCloseModal}
+				>
+					<IoClose />
+				</button>
+			</div>
+			<div className='py-5'>
 				{stationsData?.items.length ? (
 					stationsData.items.map((sensor) => (
 						<div
 							key={sensor.id}
-							className="grid grid-cols-3 gap-4 items-center p-4 text-sm border-b last:border-b-0"
+							className='grid grid-cols-3 gap-4 items-center p-4 text-sm border-b last:border-b-0'
 						>
-							<div className="font-medium text-gray-700">
+							<div className='font-medium text-gray-700'>
 								{sensor.indicator_name}
 							</div>
-							<div className="text-gray-600">
+							<div className='text-gray-600'>
 								{sensor.latest_measurement?.value ?? (
-									<span className="italic text-red-500">Brak danych</span>
+									<span className='italic text-red-500'>Brak danych</span>
 								)}
 							</div>
-							<div className="text-gray-400 text-right text-xs">
+							<div className='text-gray-400 text-right text-xs'>
 								{formatDate(sensor.latest_measurement?.timestamp)}
 							</div>
 						</div>
 					))
 				) : (
-					<div className="text-center text-gray-500 italic py-5">
+					<div className='text-center text-gray-500 italic py-5'>
 						Brak aktywnych sensorów
 					</div>
 				)}
 			</div>
 
 			{/* Link do szczegółów stacji */}
-			<div className="text-right mt-6">
+			<div className='text-right mt-6'>
 				<Link
 					href={`/stations/${station.code}`}
-					className="text-blue-500 hover:underline inline-block"
+					className='text-blue-500 hover:underline inline-block'
 				>
 					Przejdź do szczegółów stacji →
 				</Link>
